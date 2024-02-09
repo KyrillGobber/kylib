e54c6fde1ed686d06f1a619bd756984ac8ebcf90
Kyrill <kyrill@gobber.ch>
2024-02-09 14:41:23 +0100

    First example try

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import path from 'path'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     build: {
+        copyPublicDir: false,
         lib: {
             // Could also be a dictionary or array of multiple entry points
             entry: path.resolve(__dirname, "src/index.ts"),
             name: "kylib",
             // the proper extensions will be added
-            fileName: "kylib",
+            fileName: "ky-lib",
         },
         rollupOptions: {
             // make sure to externalize deps that shouldn't be bundled
@@ -25,4 +26,5 @@ export default defineConfig({
             },
         },
     },
+    plugins: [dts()]
 });
