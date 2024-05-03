329f2658198e4bc6decacfd1de10c19a75e01e67
Kyrill <kyrill@gobber.ch>
2024-05-03 19:19:36 +0200

    feat: Floatingnav story and almost finished floating nav component

--- src/components/FloatingNav.tsx
+++ src/components/FloatingNav.tsx
@@ -5,90 +5,71 @@ import { cn } from "@/lib/utils";
 import { Menu } from "lucide-react";
 import { Separator } from "./ui/separator";
 
-export enum ScrollOrLink {
-    Scroll,
-    Link,
-}
-
-export type NavigationLink = {
-    title: string;
-    href: string;
-    scrollOrLink: ScrollOrLink;
-    addClasses?: string;
-}
-
 export type FloatingNavProps = {
-    children?: React.ReactNode;
-    navigationLinks?: NavigationLink[];
+    children?: JSX.Element[];
+    mobileChildren?: JSX.Element[];
     mobileAddition?: JSX.Element;
 };
 
 export const FloatingNav = ({
     children,
-    navigationLinks,
+    mobileChildren,
+    mobileAddition,
 }: FloatingNavProps) => {
     const { isMobile } = useIsMobile();
 
-    const getNavElements = () => {
-        if (children) return children;
-        if (navigationLinks) {
-            return (
-                // TODO
-            );
-        }
-    };
-
-    if (!children && !navigationLinks) {
-        return null;
-    }
-
-    if (isMobile) {
-        return <MobileDrawerNav children={children} />;
-    } else {
+    if (isMobile && mobileChildren) {
+        return (
+            <MobileDrawerNav
+                mobileChildren={mobileChildren}
+                mobileAddition={mobileAddition}
+            />
+        );
+    } else if (!isMobile && children) {
         return (
             <div className="flex flex-col gap-2 fixed sm:bottom-10 right-8 z-90 cursor-pointer">
-                {getNavElements()}
+                {children}
             </div>
         );
-    }
+    } else return null;
 };
 
 const MobileDrawerNav = ({
-    children,
-    navigationLinks,
-    mobileAddition
-}: FloatingNavProps) => {
+    mobileChildren,
+    mobileAddition,
+}: {
+    mobileChildren: JSX.Element[];
+    mobileAddition?: JSX.Element;
+}) => {
     const [isDrawerOpen, setDrawerOpen] = useState(false);
 
     return (
-        <Drawer preventScrollRestoration open={isDrawerOpen} onOpenChange={setDrawerOpen}>
+        <Drawer
+            preventScrollRestoration
+            open={isDrawerOpen}
+            onOpenChange={setDrawerOpen}
+        >
             <DrawerTrigger
                 onClick={() => setDrawerOpen(true)}
                 className={cn(
-                    "bg-secondary w-screen sticky h-16 z-50 bottom-0 shd:hidden flex flex-row justify-center items-center"
+                    "bg-secondary w-screen sticky h-16 z-50 bottom-0 shd:hidden flex flex-row justify-center items-center",
                 )}
             >
                 <Menu className="scale-150 self-center" color="white" />
             </DrawerTrigger>
-            <DrawerContent className="flex flex-col gap-4 bg-ninaBlue text-ninaBeige">
-                {Links.map((link) => (
-                    <a
-                        key={link.name}
-                        className={cn(buttonVariants({ variant: "ghost" }), 'cursor-pointer')}
-                        onClick={() => {
-                            setDrawerOpen(false);
-                            setTimeout(() => handleOnLinkClick(link.id), 350);
-                        }}
-                    >
-                        {link.name}
-                    </a>
-                ))}
-                <Separator />
-                <div className="flex flex-col items-center">
-                    <p className="font-bold">Terminbuchung</p>
-                    <p>Tel. 071 411 12 81</p>
-                </div>
+            <DrawerContent className="flex flex-col gap-4">
+                {mobileChildren && (
+                    <div className="flex flex-col gap-2 cursor-pointer">
+                        {mobileChildren.map((child) => child)}
+                    </div>
+                )}
+                {mobileAddition && (
+                    <>
+                        <Separator />
+                        {mobileAddition}
+                    </>
+                )}
             </DrawerContent>
-        </Drawer >
+        </Drawer>
     );
 };
---
+++ src/stories/FloatingNav.stories.tsx
@@ -0,0 +1,62 @@
+import { FloatingNav } from "@/components/FloatingNav";
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof FloatingNav> = {
+  component: FloatingNav,
+};
+
+export default meta;
+type Story = StoryObj<typeof FloatingNav>;
+
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/api/csf
+ * to learn how to use render functions.
+ */
+export const Null: Story = {
+  render: () => <FloatingNav />,
+};
+
+const floatingNavChildren = [
+  <a className={cn(buttonVariants({ variant: "secondary" }))} href="#">
+    Home
+  </a>,
+  <a className={cn(buttonVariants({ variant: "secondary" }))} href="#">
+    zwai
+  </a>,
+  <a className={cn(buttonVariants({ variant: "secondary" }))} href="#">
+    drai
+  </a>,
+  <a className={cn(buttonVariants({ variant: "secondary" }))} href="#">
+    fier
+  </a>,
+];
+
+const floatingNavMobileChildren = [
+  <a className={cn(buttonVariants({ variant: "ghost" }))} href="#">
+    Home
+  </a>,
+  <a className={cn(buttonVariants({ variant: "ghost" }))} href="#">
+    zwi
+  </a>,
+  <a className={cn(buttonVariants({ variant: "ghost" }))} href="#">
+    drai
+  </a>,
+  <a className={cn(buttonVariants({ variant: "ghost" }))} href="#">
+    fier
+  </a>,
+];
+
+const mobileAddition = <div>stuff</div>;
+
+export const Primary: Story = {
+  render: () => (
+    <FloatingNav
+      children={floatingNavChildren}
+      mobileChildren={floatingNavMobileChildren}
+      mobileAddition={mobileAddition}
+    />
+  ),
+};
