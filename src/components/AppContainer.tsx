a625f161ca66fd9a61fee6433206504bd9152fdd
Kyrill <kyrill@gobber.ch>
2024-06-19 18:38:46 +0200

    feat: AddClasses for appcontainer

--- src/components/AppContainer.tsx
+++ src/components/AppContainer.tsx
@@ -1,11 +1,27 @@
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
 
-export const AppContainer = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className={cn("relative h-full font-sans antialiased")}>
-      <main className="relative flex flex-col min-h-screen gap-8">
-        {children}
-      </main>
-    </div>
-  );
+export const AppContainer = ({
+    children,
+    bgColorCssString,
+    addMainClasses,
+}: {
+    children: React.ReactNode;
+    bgColorCssString?: string;
+    addMainClasses?: string;
+}) => {
+    return (
+        <div
+            className={cn(
+                'relative h-full font-sans antialiased',
+                bgColorCssString
+            )}
+        >
+            <main className={cn(
+                "relative flex flex-col min-h-screen gap-8",
+                addMainClasses
+            )}>
+                {children}
+            </main>
+        </div>
+    );
 };
