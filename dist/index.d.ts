bdebe3c4232872b24ac7c0fe30acb2bcff1b59e2
Kyrill <kyrill@gobber.ch>
2024-06-23 19:49:32 +0200

    chore: changed improt statement

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -8,5 +8,5 @@ export { ThemeProvider } from './components/theme-provider';
 export { AppContainer } from './components/AppContainer';
 export { HeroWrapper } from './components/organisms/HeroWrapper';
 export { Carousel, CarouselItem, CarouselPrevious, CarouselNext, CarouselContent } from "./components/ui/carousel";
-export { getRandomTextGradient, TextGradients } from "./lib/utils";
+export { getRandomTextGradient, TextGradients } from './lib/utils';
 //# sourceMappingURL=index.d.ts.map
